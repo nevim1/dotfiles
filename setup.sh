49f51b7e37db939f792ad74ea1ac741b7b7dabfd
nevim <ondrej.lupisek@gmail.com>
2026-05-04 01:44:30 +0200

    cleaning up and adding comments in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,20 +1,26 @@
-PWD=$(pwd)
-for i in $(ls -A); do 
+#!/usr/bin/env bash
+
+# go over all files in this repo
+for i in $(ls -A); do
+	# skip markdowns, git files and this script
 	mi=$( echo $i | sed -r -e s/[^\(tmux\)]\.conf$// -e s/.*\.md// -e s/^\.git\(i.*$\|$\)// -e s/setup\.sh// )
 	if [[ ! -z $mi ]] then
+		# check if it already exists in home folder
 		if [ -f ~/$mi ]; then
 			echo $mi was found in your home dir.
-			if read -p "Replace it? (y/N) " conf && [[ $conf == [yY] || $conf == [yY][eE][sS] ]]; then
-				mv ~/$mi "$PWD"/"$mi".old
+			if read -p "Replace it? (y/N) " conf && [[ "$conf" =~ ^[yY]([eE][sS]?)?$ ]]; then
+				mv ~/$mi ~/"$mi".old
 				ln -s "$PWD"/$mi ~/$mi
-				echo the old version was moved to "$PWD"/"$mi".old
+				echo the old version was moved to ~/"$mi".old
 			fi
 		else
 			ln -s "$PWD"/$mi ~/$mi
 		fi
-	fi	
+	fi
 done
 
+# if there isn't plug.vim make it
+# if it wasn't there vim plugins would break
 if [[ ! -f ~/.vim/autoload/plug.vim ]]; then
 	curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
